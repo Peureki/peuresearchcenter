38e055d45acee8558c729368dae085220626c730
Matt Nguyen <newyenmatt@gmail.com>
2021-08-25 23:51:34 -0500

    Quick update

--- index.php
+++ index.php
@@ -139,6 +139,10 @@
 
 			<div class = "small-description" style = "overflow-y: scroll; height: 500px; width: 80%; margin-left: auto; margin-right: auto;">
 				<p1>
+					<span style = "font-weight: bold;">August 25, 2021</span>
+				<br>
+					- Fixed mobile site for <a href = "./resource/experience.php" target = "_blank"><u> Experience</u></a>
+				<br><br>
 					<span style = "font-weight: bold;">August 22, 2021</span>
 				<br>
 					- Fixed some coloring in tooltips in <a href = "./research/refinement.php" target = "_blank"><u> Refinement</u></a> for dark mode
