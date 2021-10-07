2277e401aadd7e889d5fae0500689c20c8bfbd20
Matt Nguyen <newyenmatt@gmail.com>
2021-10-06 23:09:55 -0500

    Fixed footer

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
 <div class = "footer">
 	<div class = "inner-footer">
 		<h1> Created By </h1>
-		<a href = "<?php echo $base;?>resources/about-me.html"> Peureki.3647 |</a>
-		<a href = "<?php echo $base;?>resources/about-me.html"> Peureki#5452 |</a>
+		<a href = "<?php echo $base;?>/resources/about-me.php"> Peureki.3647 |</a>
+		<a href = "<?php echo $base;?>/resources/about-me.php"> Peureki#5452 |</a>
 		<a href = "mailto: peureseachcenter@gmail.com"> peuresearchcenter@gmail.com </a>
 		<h1 style = "margin-top: 10px;"> Artwork </h1>
 		<a href = "https://www.twitter.com/tiffymew"> @tiffymew </a>
@@ -13,7 +13,7 @@
 
 	<div class = "inner-footer">
 		<h1> Support </h1>
-		<a href="<?php echo $base;?>/supporters.html" style = "margin-bottom: 10px;"> List of supporters </a>
+		<a href="<?php echo $base;?>/supporters.php" style = "margin-bottom: 10px;"> List of supporters </a>
 		<br><a href="https://www.patreon.com/peureki" target = "_blank"> Patreon </a>
 		<br><a href="https://www.paypal.me/peureki" target = "_blank">Paypal </a>
 	</div>
